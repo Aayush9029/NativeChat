31c21cda4c59fe9b29efa1f55b9b62b0f6f2f91a
Aayush9029 <aayushpokharel36@gmail.com>
2021-10-30 13:12:57 -0400

    Initial Commit

---
+++ NativeChat/NativeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeChatApp.swift
+//  NativeChat
+//
+//  Created by Aayush Pokharel on 2021-10-30.
+//
+
+import SwiftUI
+
+@main
+struct NativeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
